cc70027503c1939dae4608e2caee714b28dfdd09
huolongguo1O <121071167+huolongguo1O@users.noreply.github.com>
2024-08-11 10:47:07 +0000

    1

--- lakefile.lean
+++ lakefile.lean
@@ -1,13 +1,15 @@
 import Lake
 open Lake DSL
 
-package «lean4-example» {
-  -- add package configuration options here
-}
+package "BProver" where
+  -- Settings applied to both builds and interactive editing
+  leanOptions := #[
+    ⟨`pp.unicode.fun, true⟩ -- pretty-prints `fun a ↦ b`
+  ]
+  -- add any additional package configuration options here
+
+require "leanprover-community" / "mathlib"
 
 @[default_target]
-lean_lib «Lean4Example» {
-  -- add library configuration options here
-}
-require mathlib from git
-  "https://github.com/leanprover-community/mathlib4"
+lean_lib «BProver» where
+  -- add any library configuration options here
