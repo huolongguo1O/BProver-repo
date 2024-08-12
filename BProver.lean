352ec328640cf1295043d9af9c23c709f601de9d
huolongguo1O <121071167+huolongguo1O@users.noreply.github.com>
2024-08-12 12:14:20 +0800

    Update BProver.lean

--- BProver.lean
+++ BProver.lean
@@ -1,4 +1,30 @@
 -- This module serves as the root of the `BProver` library.
 -- Import modules here that should be built as part of the library.
 import BProver.Basic
-import Basic
+/-
+Copyright (c) 2020 Kevin Lacker. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Kevin Lacker
+-/
+import Mathlib.Tactic.Ring
+import Mathlib.Data.Nat.Prime.Basic
+
+/-!
+# IMO 1959 Q1
+
+Prove that the fraction `(21n+4)/(14n+3)` is irreducible for every natural number `n`.
+
+Since Lean doesn't have a concept of "irreducible fractions" per se, we just formalize this
+as saying the numerator and denominator are relatively prime.
+-/
+
+
+open Nat
+
+
+
+theorem calculation (n k : ℕ) (h1 : k ∣ 21 * n + 4) (h2 : k ∣ 14 * n + 3) : k ∣ 1 :=
+  have h3 : k ∣ 2 * (21 * n + 4) := h1.mul_left 2
+  have h4 : k ∣ 3 * (14 * n + 3) := h2.mul_left 3
+  have h5 : 3 * (14 * n + 3) = 2 * (21 * n + 4) + 1 := by ring
+  (Nat.dvd_add_right h3).mp (h5 ▸ h4)
